536640ea22c0deafd85cd62d621c5470572250e0
abdurahmonovAbdulvahhob <abdulvahhobabdurahmonov@gmail.com>
2024-10-24 10:01:53 +0500

    refresh token guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,66 +15,69 @@ import { UpdateAuthDto } from "./dto/update-auth.dto";
 import { Response } from "express";
 import { SignInDto } from "./dto";
 import { CookieGetter } from "../common/decorators/cookie_getter.decorator";
-import { AccessTokenGuard } from "../common/guards";
+import { AccessTokenGuard, RefreshTokenGuard } from "../common/guards";
 import { Public } from "../common/decorators";
 
-  @UseGuards(AccessTokenGuard)
-  @Controller("auth")
-  export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+@UseGuards(AccessTokenGuard)
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-    @Public()
-    @Post("signup")
-    async signup(
-      @Body() createAuthDto: CreateAuthDto,
-      @Res({ passthrough: true }) res: Response
-    ) {
-      return this.authService.signup(createAuthDto, res);
-    }
-
-    @Public()
-    @Post("signin")
-    async signin(
-      @Body() signInDto: SignInDto,
-      @Res({ passthrough: true }) res: Response
-    ) {
-      return this.authService.signin(signInDto, res);
-    }
-
-    @UseGuards(AccessTokenGuard)
-    @Post("signout")
-    async signOut(
-      @CookieGetter("refresh_token") refreshToken: string,
-      @Res({ passthrough: true }) res: Response
-    ) {
-      return this.authService.signout(refreshToken, res);
-    }
-
-    @Post("refreshtoken")
-    async refreshToken(
-      @Res({ passthrough: true }) res: Response,
-      @CookieGetter("refresh_token") refresh_token: string
-    ) {
-      return this.authService.refreshTokens(refresh_token, res);
-    }
-
-    @Get("get")
-    findAll() {
-      return this.authService.findAll();
-    }
-
-    @Get("get/:id")
-    findOne(@Param("id") id: string) {
-      return this.authService.findOne(+id);
-    }
-
-    @Patch("update/:id")
-    update(@Param("id") id: string, @Body() updateAuthDto: UpdateAuthDto) {
-      return this.authService.update(+id, updateAuthDto);
-    }
-
-    @Delete("delete/:id")
-    remove(@Param("id") id: string) {
-      return this.authService.remove(+id);
-    }
+  @Public()
+  @Post("signup")
+  async signup(
+    @Body() createAuthDto: CreateAuthDto,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    return this.authService.signup(createAuthDto, res);
   }
+
+  @Public()
+  @Post("signin")
+  async signin(
+    @Body() signInDto: SignInDto,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    return this.authService.signin(signInDto, res);
+  }
+
+  @Public()
+  @UseGuards(RefreshTokenGuard)
+  @Post("signout")
+  async signOut(
+    @CookieGetter("refresh_token") refreshToken: string,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    return this.authService.signout(refreshToken, res);
+  }
+
+  @Public()
+  @UseGuards(RefreshTokenGuard)
+  @Post("refreshtoken")
+  async refreshToken(
+    @Res({ passthrough: true }) res: Response,
+    @CookieGetter("refresh_token") refresh_token: string
+  ) {
+    return this.authService.refreshTokens(refresh_token, res);
+  }
+
+  @Get("get")
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @Get("get/:id")
+  findOne(@Param("id") id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  @Patch("update/:id")
+  update(@Param("id") id: string, @Body() updateAuthDto: UpdateAuthDto) {
+    return this.authService.update(+id, updateAuthDto);
+  }
+
+  @Delete("delete/:id")
+  remove(@Param("id") id: string) {
+    return this.authService.remove(+id);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { PrismaModule } from "../prisma/prisma.module";
 import { JwtModule } from "@nestjs/jwt";
-import { AccessTokenStrategy } from "../common/strategies";
+import { AccessTokenStrategy, RefreshTokenCookieStrategy } from "../common/strategies";
 
 @Module({
   imports: [JwtModule.register({}), PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService, AccessTokenStrategy],
+  providers: [AuthService, AccessTokenStrategy,RefreshTokenCookieStrategy],
 })
 export class AuthModule {}
